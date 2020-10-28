2e490802c389bbe392cdc6e186333c1a2b2bd972
Zheronimo <kirichenko.e.o@gmail.com>
2020-10-29 00:00:02 +0200

    Исправлены шрифты и ошибки при скролинге страницы. Добавлена адаптация футера

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	// ==============================
 	// Check scrollbar width
 	// ==============================
@@ -6,7 +7,7 @@ $(document).ready(function(){
 		var style = {"max-height":"100px", "overflow":"scroll", "position":"absolute"},
 			wrapper = $("<div id='scroll_bar_check_A'></div>").css(style),
 			inner = $("<div id='scroll_bar_check_B'></div>"),
-			stretcher = $("<img src='/assets/img/force-scroll.png'/>"),
+			stretcher = $("<img src='/static/img/force-scroll.png'/>"),
 			scrollBarWidth = 0;
 
 		wrapper.append(stretcher).append(inner);
@@ -47,7 +48,7 @@ $(document).ready(function(){
 
 	/* Parallax */
 	var section = $(".hero");
-	$(window).scroll(function(){
+	$(window).on('scroll', function(){
 		var scrollTop = -($(window).scrollTop()),
 				speed = section.data("speed"),
 				coords = "50%" + scrollTop / speed + "px"
@@ -85,86 +86,87 @@ $(document).ready(function(){
 			}
 		});
 	});
-	/* **
-	Блог плавная прокрутка
+
+	/* 
+	Блог и плавная прокрутка
 	*/
-	function blogNavigation() {
-		// ==============================
-		// Save needed elements
-		// ==============================
-		var active_id,
-			additional_offset = 60,
-			menu = $(".blog__sidebar"),
-			menu_items = menu.find("li a"),
-			scroll_items = menu_items.map(function(){
-				var item = $($(this).attr("href"));
-				if (item.length) return item;
-			});
 
-
-
-		// =====================================
-		// Sidemenu link smoothscroll handler
-		// =====================================
-		menu_items.click(function(e){
-			var href = $(this).attr("href"),
-				offsetTop = (href === "#") ? 0 : $(href).offset().top - additional_offset;
-
-			e.preventDefault();
-
-			$(".blog-menu__item").removeClass('blog-menu__item_active');
-			$(this).closest(".blog-menu__item").addClass('blog-menu__item_active');
-
-			$("html, body").stop().animate({ 
-				scrollTop: offsetTop
-			}, 700, "swing");
-		});
-
-
-
-		// ======================================================
-		// Articles scroll-spy handler and sidemenu positioning
-		// ======================================================
-		$(window).scroll(function() {
-			var fromTop = $(this).scrollTop(),
-				blog_nav_offset = $(".blog__sidebar").offset().top,
-				blog_nav_limit = $(".blog__footer").offset().top - $(".blog-menu").outerHeight(),
-				current = scroll_items.map(function(){
-					if ($(this).offset().top <= fromTop+additional_offset+1){
-						return this;
+	if(window.location.toString().indexOf('blog.html')>0)
+		{
+			function blogNavigation() {
+				// ==============================
+				// Save needed elements
+				// ==============================
+				var active_id,
+					additional_offset = 60,
+					menu = $(".blog__sidebar"),
+					menu_items = menu.find("li a"),
+					scroll_items = menu_items.map(function(){
+						var item = $($(this).attr("href"));
+						if (item.length) return item;
+					});
+		
+				// =====================================
+				// Sidemenu link smoothscroll handler
+				// =====================================
+				menu_items.on('click', function(e){
+					var href = $(this).attr("href"),
+						offsetTop = (href === "#") ? 0 : $(href).offset().top - additional_offset;
+		
+					e.preventDefault();
+		
+					$(".blog-menu__item").removeClass('blog-menu__item_active');
+					$(this).closest(".blog-menu__item").addClass('blog-menu__item_active');
+		
+					$("html, body").stop().animate({ 
+						scrollTop: offsetTop
+					}, 700, "swing");
+				});
+		
+				// ======================================================
+				// Articles scroll-spy handler and sidemenu positioning
+				// ======================================================
+				$(window).on('scroll', function() {
+					var fromTop = $(this).scrollTop(),
+						blog_nav_offset = $(".blog__sidebar").offset().top,
+						blog_nav_limit = $(".blog__footer").offset().top - $(".blog-menu").outerHeight(),
+						current = scroll_items.map(function(){
+							if ($(this).offset().top <= fromTop+additional_offset+1){
+								return this;
+							}
+						});
+		
+					// Sidemenu behaviour
+					if(fromTop >= blog_nav_limit && $(window).width() > window.hm.tabletSize) {
+						$(".blog-menu").css({"position":"absolute", "top":blog_nav_limit + "px"});
+					} else if (fromTop >= blog_nav_offset && $(window).width() > window.hm.tabletSize) {
+						$(".blog-menu").css({"position":"fixed", "top":0});
+						$(".blog-menu").addClass("nav-fixed");
+					} else {
+						$(".blog-menu").css({"position":"relative"});
+						$(".blog-menu").removeClass("nav-fixed");
 					}
 				});
+		
+				// =====================================
+				// Adaptive sidemenu checker
+				// =====================================
+			/* 	$(window).resize(function() {
+					if($(window).width() <= window.hm.tabletSize){
+						$(".blog-navigation__wrapper").removeClass("nav-fixed");
+						$(".blog-navigation__wrapper").css({"position":"relative"});
+					} else {
+						if($("body").scrollTop() >= $(".blog").offset().top){
+							$(".blog-navigation__wrapper").css({"position":"fixed", "top":0});
+							$(".blog-navigation__wrapper").addClass("nav-fixed");
+						}
+					}
+				}); */
+			};
+			blogNavigation();
+		};
 
-			// Sidemenu behaviour
-			if(fromTop >= blog_nav_limit && $(window).width() > window.hm.tabletSize) {
-				$(".blog-menu").css({"position":"absolute", "top":blog_nav_limit + "px"});
-			} else if (fromTop >= blog_nav_offset && $(window).width() > window.hm.tabletSize) {
-				$(".blog-menu").css({"position":"fixed", "top":0});
-				$(".blog-menu").addClass("nav-fixed");
-			} else {
-				$(".blog-menu").css({"position":"relative"});
-				$(".blog-menu").removeClass("nav-fixed");
-			}
-		});
-
-		// =====================================
-		// Adaptive sidemenu checker
-		// =====================================
-	/* 	$(window).resize(function() {
-			if($(window).width() <= window.hm.tabletSize){
-				$(".blog-navigation__wrapper").removeClass("nav-fixed");
-				$(".blog-navigation__wrapper").css({"position":"relative"});
-			} else {
-				if($("body").scrollTop() >= $(".blog").offset().top){
-					$(".blog-navigation__wrapper").css({"position":"fixed", "top":0});
-					$(".blog-navigation__wrapper").addClass("nav-fixed");
-				}
-			}
-		}); */
-	};
-	blogNavigation();
-
-
+	
 	/* SLIDER */
 	(function () {
 		// slider

eebeade4c5f42332a6b2ac6d842ac068e036d9da
Zheronimo <kirichenko.e.o@gmail.com>
2020-10-31 14:47:00 +0200

    Закончен адаптив

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php 
+	$recepient = '';
+	$sitename = 'portfolio';
+
+	$name = trim($_POST['name']);
+	$phone = trim($_POST['tel']);
+	$city = trime($_POST['cityname']);
+	$massege = "Имя: $name \nТелефон: $phone \nГород: $city";
+
+	$pagetitle = "Новая заявка с сайта \"$sitename\"";
+	mail($recepient, $pagetitle, $massege, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
